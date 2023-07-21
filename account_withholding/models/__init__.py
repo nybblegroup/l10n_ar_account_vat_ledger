f550ed50278f791a75ebd803520c86653a1bb888
Ivan <capa0soluciones@gmail.com>
2023-07-21 11:53:34 -0300

    repo clean

--- account_withholding/models/__init__.py
+++
@@ -1,8 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import account_tax
-from . import account_payment
-from . import account_chart_template
-from . import account_payment_method
